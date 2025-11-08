358491e1ca06795fc7a87a969cce65f45a9f4248
Junxiao-Liao <huruyuanxingke66@gmail.com>
2025-11-08 21:13:24 +1300

    Add project files.

---
+++ MergeImages.UI/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MergeImages.UI.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
